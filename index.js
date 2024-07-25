6f4e6f211db05f493dad68c48cfd3ba3d9cb172b
Majak Deng <majakbanks.deng>
2024-07-25 15:58:11 +0300

    modified

---
+++ index.js
@@ -0,0 +1,10 @@
+// 1. Function expression for divide
+const divide = function(a, b) {
+    return a / b;
+  };
+  
+  // 2. Arrow function for square
+  const square = (num) => num * num;
+  
+  // 3. Arrow function for add
+  const add = (a, b) => a + b;
